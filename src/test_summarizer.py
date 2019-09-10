fe942fcd467a7d5ba3270f0a6e4f655a791cedd2
ducanhnguyen <anhnd.56@gmail.com>
2019-09-10 14:26:10 +0700

    export image on ubuntu

--- src/test_summarizer.py
+++ src/test_summarizer.py
@@ -1,11 +1,12 @@
 import csv
 import logging
-
+import platform
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy.misc
 
+import matplotlib
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
@@ -78,7 +79,12 @@ def draw_figure(model_object, seed, original_prediction, modified_prediction, pn
     # if the input model is in range of [0..1] and the value of pixel in image is in [0..255], we need to scale the image
     new_image /= 255
 
-    scipy.misc.toimage(new_image, cmin=0.0, cmax=1).save(png_new_image_path)
+    if 'ubuntu' in platform.platform().lower():
+        # scipy.misc.imsave works on macosx but does not work on ubuntu 17 + python 3.6
+        # solution: on ubuntu 17, use matplotlib.image.imsave instead
+        matplotlib.image.imsave(png_new_image_path, new_image)
+    else:
+        scipy.misc.imsave(new_image, cmin=0.0, cmax=1).save(png_new_image_path)
 
     fig = plt.figure()
     nrow = 1
