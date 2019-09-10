35eb5b98e5f8313a843692528dbae354e15d275c
ducanhnguyen <iai19101993>
2019-09-10 17:12:19 +0700

    mnist: train on the whole train set

--- src/saved_models/mnist_ann_keras.py
+++ src/saved_models/mnist_ann_keras.py
@@ -4,6 +4,11 @@ from keras.models import Sequential
 from src.abstract_dataset import *
 import pandas as pd
 
+'''
+Data set: https://www.kaggle.com/c/digit-recognizer
+Train on 42000 samples.
+Test the model on kaggle: accuracy on test set = 0.95957
+'''
 class MNIST(abstract_dataset):
 
     def __init__(self):
@@ -11,13 +16,14 @@ class MNIST(abstract_dataset):
 
     def read_data(self, trainset_path, testset_path):
         X = pd.read_csv(trainset_path).to_numpy()
-        limit = 40000
+        self.set_Xtrain(X[:, 1:]/255)
+        self.set_ytrain(X[:, 0].reshape(-1))
 
-        self.set_Xtrain(X[:limit, 1:]/255)
-        self.set_ytrain(X[:limit, 0].reshape(-1))
-
-        self.set_Xtest(X[limit:, 1:]/255)
-        self.set_ytest(X[limit:, 0].reshape(-1))
+        # we do not know the label of samples in test set
+        # to compute the accuracy of the model, we will upload the prediction on kaggle
+        X = pd.read_csv(testset_path).to_numpy()
+        self.set_Xtest(X/255)
+        #self.set_ytest(None)
 
         return self.get_Xtrain(), self.get_ytrain(), self.get_Xtest(), self.get_ytest()
 
@@ -36,6 +42,17 @@ class MNIST(abstract_dataset):
         self.set_model(model)
         return model
 
+    def predict_on_testset_kaggle(self):
+        model = self.get_model()
+        Ytest = np.argmax(model.predict(self.get_Xtest()), axis=1)
+
+        import csv
+        with open('/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/src/saved_models/mnist_test_result.csv', mode='w') as f:
+            seed = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            seed.writerow(['ImageId', 'Label'])
+            for idx, item in enumerate(Ytest):
+                seed.writerow([idx + 1, item]) # index starts from 1
+        print(Ytest)
 
 if __name__ == '__main__':
     # train model
@@ -50,12 +67,13 @@ if __name__ == '__main__':
     model = mnist.load_model(weight_path='../saved_models/mnist_ann_keras.h5', structure_path='../saved_models/mnist_ann_keras.json',
                              trainset_path='../../digit-recognizer/train.csv')
     mnist.read_data(trainset_path='../../digit-recognizer/train.csv', testset_path='../../digit-recognizer/test.csv')
-    print(model.layers)
+    print(f'model.layers = {model.layers}')
+    mnist.predict_on_testset_kaggle()
 
     # plot an observation
     import matplotlib.pyplot as plt
     x_train, y_train = mnist.get_an_observation(index=516)
     img = x_train.reshape(28, 28)
-    print(y_train)
     plt.imshow(img, cmap='gray')
+    plt.title(f'A sample')
     plt.show()
