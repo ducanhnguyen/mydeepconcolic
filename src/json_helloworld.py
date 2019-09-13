6da829c524b1cff0cfd040a3feb84dc7045b0cf0
ducanhnguyen <iai19101993>
2019-09-13 09:05:22 +0700

    [add] get config of json

--- src/json_helloworld.py
+++ src/json_helloworld.py
@@ -1,12 +1,18 @@
 import json
 
 def get_config(attributes, config_path = './config.json'):
+    DATASET_SIGNAL = '{dataset}'
     with open(config_path, 'r') as f:
         config = json.load(f)
 
         con = config
         for attribute in attributes:
             con = con[attribute]
+
+        if isinstance(con,str):
+            if DATASET_SIGNAL in con:
+                con = con.replace(DATASET_SIGNAL, get_config(['dataset']))
+
         return con
 
 if __name__ == '__main__':
