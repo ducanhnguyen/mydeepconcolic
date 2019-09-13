8d279393109fe3ef8f2b4c796f7c3f6026d037cc
ducanhnguyen <anhnd.56@gmail.com>
2019-09-13 13:11:00 +0700

    [add] support multiplatform

--- src/json_helloworld.py
+++ src/json_helloworld.py
@@ -1,7 +1,6 @@
 import json
 
-def get_config(attributes, config_path = './config.json'):
-    DATASET_SIGNAL = '{dataset}'
+def get_config(attributes, config_path = './config_ubuntu.json'):
     with open(config_path, 'r') as f:
         config = json.load(f)
 
@@ -10,8 +9,13 @@ def get_config(attributes, config_path = './config.json'):
             con = con[attribute]
 
         if isinstance(con,str):
+            DATASET_SIGNAL = '{dataset}'
             if DATASET_SIGNAL in con:
                 con = con.replace(DATASET_SIGNAL, get_config(['dataset']))
+
+            BASE_PATH = '{base_path}'
+            if BASE_PATH in con:
+                con = con.replace(BASE_PATH, get_config(['base_path']))
 
         return con
 
