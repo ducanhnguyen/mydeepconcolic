72d27a3069763e90e38fb69eeb8132f3656cc0a5
ducanhnguyen <iai19101993>
2019-09-17 09:27:30 +0700

    [add] normalization option

--- src/saved_models/mnist_ann_keras.py
+++ src/saved_models/mnist_ann_keras.py
@@ -7,24 +7,31 @@ import pandas as pd
 '''
 Data set: https://www.kaggle.com/c/digit-recognizer
 Train on 42000 samples.
-Test the model on kaggle: accuracy on test set = 0.95957
+
+Test the model on kaggle:
+(1) 784 units (input layer), 32 units, 16 units, 10 units 
+    accuracy on original test set = 0.95957 -> 0.95957 (original test set + adversarial samples)
 '''
 class MNIST(abstract_dataset):
 
     def __init__(self):
         super(MNIST, self).__init__()
 
-    def read_data(self, trainset_path, testset_path):
+    def read_data(self, trainset_path, testset_path, divided_by_255 = True):
         X = pd.read_csv(trainset_path).to_numpy()
-        self.set_Xtrain(X[:, 1:]/255)
-        self.set_ytrain(X[:, 0].reshape(-1))
+        if divided_by_255:
+            self.set_Xtrain(X[:, 1:] / 255)
+        else:
+            self.set_Xtrain(X[:, 1:])
+        self.set_ytrain(X[:, 0].reshape(-1).astype(int))
 
         # we do not know the label of samples in test set
         # to compute the accuracy of the model, we will upload the prediction on kaggle
         X = pd.read_csv(testset_path).to_numpy()
-        self.set_Xtest(X/255)
-        #self.set_ytest(None)
-
+        if divided_by_255:
+            self.set_Xtest(X / 255)
+        else:
+            self.set_Xtest(X)
         return self.get_Xtrain(), self.get_ytrain(), self.get_Xtest(), self.get_ytest()
 
     def create_model(self, input_shape):
@@ -42,12 +49,12 @@ class MNIST(abstract_dataset):
         self.set_model(model)
         return model
 
-    def predict_on_testset_kaggle(self):
+    def predict_on_testset_kaggle(self, path):
         model = self.get_model()
         Ytest = np.argmax(model.predict(self.get_Xtest()), axis=1)
 
         import csv
-        with open('/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/src/saved_models/mnist_test_result.csv', mode='w') as f:
+        with open(path, mode='w') as f:
             seed = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             seed.writerow(['ImageId', 'Label'])
             for idx, item in enumerate(Ytest):
@@ -60,15 +67,17 @@ if __name__ == '__main__':
     mnist.set_num_classes(10)
 
     '''
-    mnist.train_model(train=True, kernel_path='../saved_models/mnist_ann_keras.h5',  model_path='../saved_models/mnist_ann_keras.json',
-                      training_path='../../digit-recognizer/train.csv', testing_path='../../digit-recognizer/test.csv')
+    mnist.train_model(train=True, kernel_path='../saved_models/mnist_ann_keras_expansion.h5',  model_path='../saved_models/mnist_ann_keras_expansion.json',
+                      training_path='/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/dataset/digit-recognizer/train_expansion.csv',
+                      testing_path='/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/dataset/digit-recognizer/test.csv')
     '''
     # load model
-    model = mnist.load_model(weight_path='../saved_models/mnist_ann_keras.h5', structure_path='../saved_models/mnist_ann_keras.json',
-                             trainset_path='../../digit-recognizer/train.csv')
-    mnist.read_data(trainset_path='../../digit-recognizer/train.csv', testset_path='../../digit-recognizer/test.csv')
+    model = mnist.load_model(weight_path='../saved_models/mnist_ann_keras_expansion.h5', structure_path='../saved_models/mnist_ann_keras_expansion.json',
+                             trainset_path='/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/dataset/digit-recognizer/train_expansion.csv')
+    mnist.read_data(trainset_path='/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/dataset/digit-recognizer/train_expansion.csv',
+                    testset_path='/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/dataset/digit-recognizer/test.csv')
     print(f'model.layers = {model.layers}')
-    mnist.predict_on_testset_kaggle()
+    mnist.predict_on_testset_kaggle(path = '/Users/ducanhnguyen/Documents/python/pycharm/mydeepconcolic/src/saved_models/mnist_test_result.csv')
 
     # plot an observation
     import matplotlib.pyplot as plt
@@ -77,3 +86,4 @@ if __name__ == '__main__':
     plt.imshow(img, cmap='gray')
     plt.title(f'A sample')
     plt.show()
+
