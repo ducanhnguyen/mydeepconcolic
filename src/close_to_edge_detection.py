77be7d60a80726a01f51d1a7cff755f606469e56
ducanhnguyen <anhnd.56@gmail.com>
2021-03-22 16:58:26 +0700

    [add] close_to_edge_detection.py

--- src/close_to_edge_detection.py
+++ src/close_to_edge_detection.py
@@ -4,29 +4,46 @@ import matplotlib.pyplot as plt
 
 
 def close_to_edge(row_idx, col_idx, x_28_28):
+    isEdge = False
+    avg = 0
+    min = 0
     if x_28_28[row_idx, col_idx] > 0:
-        return False
+        isEdge = False
     elif col_idx - 1 >= 0 and \
             row_idx + 1 <= 27 and \
             x_28_28[row_idx, col_idx - 1] > 0 and x_28_28[row_idx + 1, col_idx - 1] > 0 \
             and x_28_28[row_idx + 1, col_idx] > 0:
-        return True
+        avg = (x_28_28[row_idx, col_idx - 1] + x_28_28[row_idx + 1, col_idx - 1] + x_28_28[
+            row_idx + 1, col_idx]) / 3
+        min = np.min([x_28_28[row_idx, col_idx - 1], x_28_28[row_idx + 1, col_idx - 1], x_28_28[
+            row_idx + 1, col_idx]])
+        isEdge = True
     elif col_idx - 1 >= 0 and \
-            row_idx - 1 >= 0  and \
+            row_idx - 1 >= 0 and \
             x_28_28[row_idx, col_idx - 1] > 0 and x_28_28[row_idx - 1, col_idx - 1] > 0 \
             and x_28_28[row_idx - 1, col_idx] > 0:
-        return True
+        avg = (x_28_28[row_idx, col_idx - 1] + x_28_28[row_idx - 1, col_idx - 1] + x_28_28[
+            row_idx - 1, col_idx]) / 3
+        min = np.min([x_28_28[row_idx, col_idx - 1], x_28_28[row_idx - 1, col_idx - 1], x_28_28[
+            row_idx - 1, col_idx]])
+        isEdge = True
     elif col_idx + 1 <= 27 and \
-            row_idx - 1 >= 0  and \
+            row_idx - 1 >= 0 and \
             x_28_28[row_idx, col_idx + 1] > 0 and x_28_28[row_idx - 1, col_idx] > 0 \
             and x_28_28[row_idx - 1, col_idx + 1] > 0:
-        return True
+        avg = (x_28_28[row_idx, col_idx + 1] + x_28_28[row_idx - 1, col_idx] + x_28_28[
+            row_idx - 1, col_idx + 1]) / 3
+        min = np.min([x_28_28[row_idx, col_idx + 1], x_28_28[row_idx - 1, col_idx], x_28_28[
+            row_idx - 1, col_idx + 1]])
+        isEdge = True
     elif col_idx + 1 <= 27 and \
-            row_idx + 1 <= 27  and \
+            row_idx + 1 <= 27 and \
             x_28_28[row_idx, col_idx + 1] > 0 and x_28_28[row_idx + 1, col_idx + 1] > 0 \
             and x_28_28[row_idx + 1, col_idx] > 0:
-        return True
-    return False
+        avg = (x_28_28[row_idx, col_idx + 1] + x_28_28[row_idx + 1, col_idx + 1] + x_28_28[row_idx + 1, col_idx]) / 3
+        min = np.min([x_28_28[row_idx, col_idx + 1], x_28_28[row_idx + 1, col_idx + 1], x_28_28[row_idx + 1, col_idx]])
+        isEdge = True
+    return isEdge, avg, min
 
 
 if __name__ == '__main__':
@@ -41,14 +58,26 @@ if __name__ == '__main__':
     x_28_28 = model_object.get_Xtrain()[5638].reshape(28, 28)  # 0..1
     x_clone = x_28_28.copy()
     max = np.max(x_28_28)
-    for row_idx in range(28):
-        for col_idx in range(28):
-            if close_to_edge(row_idx, col_idx, x_28_28):
-                x_clone[row_idx, col_idx] = 1
-            elif x_28_28[row_idx, col_idx] > 0:
-                x_clone[row_idx, col_idx] = 0.5
-            else:
-                x_clone[row_idx, col_idx] = 0
+
+    for feature_idx in range(784):
+        row_idx = int(np.floor(feature_idx / 28))
+        col_idx = feature_idx - row_idx * 28
+        close, avg, min = close_to_edge(row_idx, col_idx, x_28_28)
+        if close:
+            x_clone[row_idx, col_idx] = 1
+        elif x_28_28[row_idx, col_idx] > 0.9:
+            x_clone[row_idx, col_idx] = 0.9
+        else:
+            x_clone[row_idx, col_idx] = x_28_28[row_idx, col_idx]
+
+    # for row_idx in range(28):
+    #     for col_idx in range(28):
+    #         if close_to_edge(row_idx, col_idx, x_28_28):
+    #             x_clone[row_idx, col_idx] = 1
+    #         elif x_28_28[row_idx, col_idx] > 0:
+    #             x_clone[row_idx, col_idx] = 0.5
+    #         else:
+    #             x_clone[row_idx, col_idx] = 0
     #
     fig = plt.figure()
     fig1 = fig.add_subplot(1, 2, 1)
